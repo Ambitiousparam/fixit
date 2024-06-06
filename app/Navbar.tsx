846f1a9ebff1ef41f6a4e6dcb9254c9144e13e48
parampreet singh <parampreetsingh1092@gmail.com>
2024-06-06 16:18:02 +0530

    prisma setup

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import Link from 'next/link'
+'use client';
+import React, { useState, useEffect } from 'react';
+import Link from 'next/link';
 import { TbError404 } from "react-icons/tb";
+import { usePathname } from 'next/navigation';
 
 const Navbar = () => {
+  const currentpath = usePathname();
+  const [activePath, setActivePath] = useState(currentpath);
+
+  useEffect(() => {
+    setActivePath(currentpath);
+  }, [currentpath]);
+
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center hover:'> 
-        <Link href ="/"><TbError404 /></Link>
+    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'> 
+        <Link href="/"><TbError404 /></Link>
         <ul className='flex space-x-6 '>
-            <li  className='text-zinc-500 hover:text-black transition-colors'><Link href ="/"> Dashboard</Link></li>
-            <li  className='text-zinc-500 hover:text-black transition-colors'><Link href ="/"> Issues</Link></li>
+            <li className={`transition-colors ${activePath === '/' ? 'text-black' : 'text-zinc-500 hover:text-black'}`}>
+                <Link href="/">Dashboard</Link>
+            </li>
+            <li className={`transition-colors ${activePath === '/issues' ? 'text-black' : 'text-zinc-500 hover:text-black'}`}>
+                <Link href="/issues">Issues</Link>
+            </li>
         </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
