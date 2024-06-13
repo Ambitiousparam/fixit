ac11ef16d054b00338201273f48af581950b73ef
parampreet singh <parampreetsingh1092@gmail.com>
2024-06-13 13:03:42 +0530

    feat:dropdown  for authentication

--- app/Navbar.tsx
+++ app/Navbar.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 import { TbError404 } from "react-icons/tb";
 import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
-import { Box } from '@radix-ui/themes';
+import { Avatar, Box, Container, DropdownMenu, DropdownMenuContent, Flex, Text } from '@radix-ui/themes';
 
 const Navbar = () => {
   const currentpath = usePathname();
@@ -17,7 +17,13 @@ const Navbar = () => {
   }, [currentpath]);
 
   return (
-    <nav className='flex space-x-6 border-b mb-5 px-5 h-14 items-center'> 
+    <nav className='border-b mb-5 px-5 py-3'>
+      <Container>
+
+      
+    
+    <Flex justify="between">
+      <Flex align="center" gap="3">
         <Link href="/"><TbError404 /></Link>
         <ul className='flex space-x-6 '>
             <li className={`transition-colors ${activePath === '/' ? 'text-black' : 'text-zinc-500 hover:text-black'}`}>
@@ -27,21 +33,39 @@ const Navbar = () => {
                 <Link href="/issues/list">Issues</Link>
             </li>
         </ul>
+      </Flex>
+      <Box>
+          {status ==='authenticated' && (
+            <DropdownMenu.Root>
+              <DropdownMenu.Trigger>
+                <Avatar className='cursor-pointer' radius='full'  size="2"   src ={session.user!.image!} fallback="?"/>
 
-        <Box>
-          {status ==='authenticated' && 
+              </DropdownMenu.Trigger>
+              <DropdownMenu.Content>
+                <DropdownMenu.Label >
+                  <Text size="2">
+                  {session.user?.email}
+                  </Text>
+                </DropdownMenu.Label>
+                <DropdownMenu.Item>
+              <Link href = "/api/auth/signout">Sign out</Link>
 
-          (<Link href = "/api/auth/signout">Sign out</Link>
+                </DropdownMenu.Item>
+              </DropdownMenu.Content>
+            </DropdownMenu.Root>
+          
           
           )}
           {status ==='unauthenticated' &&(
 
             <Link href="/api/auth/signin">Login</Link>
           )}
-          
-        </Box>
+      </Box>
+    </Flex>
+    </Container>
+
     </nav>
-  );
+  );  
 };
 
 export default Navbar;
