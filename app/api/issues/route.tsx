ccc5f68cdbf5bb6d81932e5d4b3098fd5d2cc817
parampreet singh <parampreetsingh1092@gmail.com>
2024-06-15 20:25:29 +0530

    feat filter functionality

--- app/api/issues/route.tsx
+++ app/api/issues/route.tsx
@@ -1,18 +1,14 @@
-import { NextRequest, NextResponse } from "next/server";
-import { z } from 'zod';
-import prisma from "@/prisma/client";
-import { issueSchema } from "@/app/ValidationSchema";import { getServerSession } from "next-auth";
 import authOptions from "@/app/auth/authOptions";
-import { stat } from "fs";
-;
+import { issueSchema } from "@/app/ValidationSchema";
+import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+
+
 
 export async function POST(request: NextRequest) {
-
-
   const session = await getServerSession(authOptions);
-  if(!session) 
-  return NextResponse.json({},{status:401});
-
+  if (!session) return NextResponse.json({}, { status: 401 });
 
 
   const body = await request.json();
@@ -21,7 +17,7 @@ export async function POST(request: NextRequest) {
     return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newIssue = await prisma.issue.create({
-    data: { title: body.title, description: body.description }
+    data: { title: body.title, description: body.description },
   });
 
   return NextResponse.json(newIssue, { status: 201 });
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,51 +1,45 @@
-import authOptions from '@/app/auth/authOptions';
-import prisma from '@/prisma/client';
-import { Box, Flex, Grid } from '@radix-ui/themes';
-import delay from 'delay';
-import { getServerSession } from 'next-auth';
-import { notFound } from 'next/navigation';
-import AssigneeSelect from './AssigneeeSelect';
-import DeleteIssueButton from './DeleteIssueButton';
-import EditIssueButton from './EditIssueButton';
-import IssueDetails from './IssueDetails';
+import authOptions from "@/app/auth/authOptions";
+import prisma from "@/prisma/client";
+import { Box, Flex, Grid } from "@radix-ui/themes";
+import delay from "delay";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import AssigneeSelect from "./AssigneeeSelect";
+import DeleteIssueButton from "./DeleteIssueButton";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
-interface Props{
-    params:{id:string}
+interface Props {
+  params: { id: string };
 }
- 
-const IssueDetailPage = async ({params}:Props) => {
-   const session = await getServerSession(authOptions);
-    
-  
-     const issue = await prisma.issue.findUnique({
-        where:{id:parseInt(params.id)}
-    });
-    if(!issue)
-    notFound();
 
-    await delay(2000);
+const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions);
 
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!issue) notFound();
 
+  await delay(2000);
 
   return (
-    <Grid  className='ml-4' columns={{initial:"1",md:"5"}} gap="5"  >
-      <Box className='lg:col-span-4'>
-      <IssueDetails issue={issue}/>
+    <Grid className="ml-4" columns={{ initial: "1", md: "5" }} gap="5">
+      <Box className="lg:col-span-4">
+        <IssueDetails issue={issue} />
       </Box>
 
-   { session &&  (<Box>
-        <Flex direction="column" gap="4">
-          <AssigneeSelect/>
-      <EditIssueButton issueId={issue.id}/>
-      <DeleteIssueButton  issueId={issue.id}/>
-        </Flex>
-      </Box>)}
-      
-
-
-        
+      {session && (
+        <Box>
+          <Flex direction="column" gap="4">
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
-  )
-}
+  );
+};
 
-export default IssueDetailPage
+export default IssueDetailPage;
