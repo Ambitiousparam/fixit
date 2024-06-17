8072832feabb72a846e4f269a84a96c55a267fb2
parampreet singh <parampreetsingh1092@gmail.com>
2024-06-17 11:33:33 +0530

    added pagination functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import Pagination from "./components/Pagination";
 
-  export default function Home() {
+export default function Home({
+  searchParams,
+}: {
+  searchParams: { page: string };
+}) {
   return (
     <div>
-      hello world
+      <Pagination itemCount={100}  pageSize={10} currentPage={parseInt(searchParams.page)} />
     </div>
-
   );
 }
